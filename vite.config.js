c4de7da7fe0b131fb2840f5fc2957466f14198db
sandy-blaze <sandyblaze911@gmail.com>
2023-11-11 14:03:00 +0530

    sentry added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "sandyblaze",
+    project: "javascript-react"
+  })],
+
   server: {
     host: '0.0.0.0',
     port: 3000,
   },
+
+  build: {
+    sourcemap: true
+  }
 })

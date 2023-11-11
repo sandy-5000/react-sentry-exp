e588c6efb332d57e0c861a8b9e36863ac1be8bbd
sandy-blaze <sandyblaze911@gmail.com>
2023-11-11 14:24:27 +0530

    added sn

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "sandyblaze",
     project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "sandyblaze",
+    project: "javascript-react"
   })],
 
   server: {
